74da3864c67f56bb831508b46dfcb738442f8d96
zubairshafi2939 <zubairshafi2939@gmail.com>
2026-04-01 15:50:40 +0500

    leetcode 58 solved

--- leetcode58 length of last of word/second(solved in first but optimized).py
+++ leetcode58 length of last of word/second(solved in first but optimized).py
@@ -4,7 +4,7 @@ while s[end] == " ":
     end -= 1
 print(end)
 start = end
-while start >=0 and s[start] != " " :
+while start >=0 and s[start] != " " :  #while  s[start] != " " : at first i had used this. but 
     start -= 1
 print(start)
 print(end - start)
