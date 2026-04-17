9cee0f7d17e7c431ea347bdd1efb1a5f73b4f84f
zubairshafi2939 <zubairshafi2939@gmail.com>
2026-04-17 23:40:55 +0500

    leetcode 61

---
+++ leetcode 61 rotated/first.py
@@ -0,0 +1,29 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# List 1: [1, 4, 5]
+head1 = ListNode(1)
+temp = head1
+temp.next = ListNode(4)
+temp = temp.next
+temp.next = ListNode(5)
+temp = temp.next
+temp.next = ListNode(9)
+
+head = head1
+thead = head
+while thead.next:
+    print(thead.val)
+    last = thead
+    thead  = thead.next
+thead.next = head
+head = thead
+last.next = None
+print("------------")
+while head:
+    print(head.val)
+    head = head.next
+
+
